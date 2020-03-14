52fc7408c9081ff4471f73c698621bd5a54c58d6
minddal1 <jellibellss@gmail.com>
2020-03-14 18:57:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+ 
+app.use(express.json());
+
+app.use(express.static(__dirname));
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+const server = app.listen(port, (error) => {
+    if (error) {
+        console.log("Error starting the server");
+    }
+    console.log("This server is running on port", server.address().port);
+});
+ 
+app.get("/frontpage", (req, res) => {
+  return res.sendFile(__dirname + "/public/frontpage.html") 
+})
+
+app.get("/jquery", (req, res) => {
+    return res.sendFile(__dirname + "/public/jquery.html")
+})
+
+app.get("/npm", (req, res) => {
+    return res.sendFile(__dirname + "/public/npm.html")
+})
+
+app.get("/js", (req, res) => {
+    return res.sendFile(__dirname + "/public/js.html")
+})
